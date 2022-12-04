b696267b053a3786b92f833f4a9c94d7446f674b
leandro_tercette <leandro_tercette@gmail.com>
2022-12-04 20:33:09 -0300

    managing a List os Users via State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './Components/Users/AddUser';
+import UserList from './Components/Users/UserList';
+
 
 
 function App() {
+const [userList, setUsersList ] = useState([]);
+
+const addUserHandler = (uName, uAge) => {
+  setUsersList((prevUsersList) => {
+    return [...prevUsersList, {name : uName, age : uAge, id: Math.random().toString() }];
+  })
+}
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users = {userList}/>
     </div>
   );
 }
